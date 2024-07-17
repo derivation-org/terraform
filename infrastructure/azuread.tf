f0778843f43abe1d40fa657106c7bb6037de3a27
Dispatcher11 <88880889+Dispatcher11@users.noreply.github.com>
2024-07-17 07:30:34 +0300

    [UPDATE]AKS Writer role

--- infrastructure/azuread.tf
+++ infrastructure/azuread.tf
@@ -10,6 +10,18 @@ resource "azurerm_role_assignment" "aks_rbac_cluster_admin" {
   scope                = azurerm_kubernetes_cluster.this.id
 }
 
+# Create AKS Azure AD writers group & assignment
+resource "azuread_group" "aks_writer" {
+  display_name     = "AKS Cluster Writers"
+  security_enabled = true
+}
+
+resource "azurerm_role_assignment" "aks_rbac_writer" {
+  principal_id         = azuread_group.aks_writer.object_id
+  role_definition_name = "Azure Kubernetes Service RBAC Writer"
+  scope                = azurerm_kubernetes_cluster.this.id
+}
+
 # Create AKS Azure AD readers group & assignment
 resource "azuread_group" "aks_readers" {
   display_name     = "AKS Cluster Readers"
